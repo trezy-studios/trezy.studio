c95fac1cf02147a04aaa2eb7389eb5e519a36ff2
Trezy <t@trezy.com>
2023-01-16 22:23:05 -0600

    add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -44,6 +44,24 @@ export default function App(props) {
 				<meta
 					content={'width=device-width, initial-scale=1, maximum-scale=1'}
 					name={'viewport'} />
+
+				<link
+					rel={'apple-touch-icon'}
+					sizes={'180x180'}
+					href={'/favicon/apple-touch-icon.png'} />
+				<link
+					rel={'icon'}
+					type={'image/png'}
+					sizes={'32x32'}
+					href={'/favicon/favicon-32x32.png'} />
+				<link
+					rel={'icon'}
+					type={'image/png'}
+					sizes={'16x16'}
+					href={'/favicon/favicon-16x16.png'} />
+				<link
+					rel={'manifest'}
+					href={'/site.webmanifest'} />
 			</NextHead>
 
 			<Component
