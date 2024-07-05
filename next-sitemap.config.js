cc4a55d4b274951a419671d12621bdb7477c6e9d
Trezy <tre@trezy.com>
2024-07-05 11:53:25 -0500

    update the URL for the sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+	changefreq: 'daily',
+	priority: 0.7,
+	sitemapSize: 5000,
+	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		additionalSitemaps: [],
+	},
+}
